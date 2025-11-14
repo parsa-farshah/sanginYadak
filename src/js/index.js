d100977636a9e90cd7b6e49fac3670e8d56aa392
parsa <parsafarashah@gmail.com>
2025-11-15 00:34:15 +0330

    feat: add question complete

--- src/js/index.js
+++ src/js/index.js
@@ -23,3 +23,26 @@ $closeMenuBtn.addEventListener("click", () => {
   $navMob.classList.remove("left-[30%]");
   $navMob.classList.add("left-[150%]");
 });
+
+/////////////////////////////////////////////////////////////// accordion
+
+let $accordionWrapper = document.querySelector("#accordionWrapper");
+let $accordionP = document.querySelectorAll("#accordionWrapper>div>p");
+
+function clickAccor(s) {
+  $accordionP.forEach((val) => {
+    val.classList.remove("visible");
+    val.classList.add("invisible");
+    val.classList.remove("h-fit");
+    val.classList.remove("px-4");
+    val.classList.remove("py-5");
+    val.classList.add("h-0");
+  });
+  let $p = s.parentElement.children[1];
+  $p.classList.remove("h-0");
+  $p.classList.remove("invisible");
+  $p.classList.add("visible");
+  $p.classList.add("h-fit");
+  $p.classList.add("px-4");
+  $p.classList.add("py-5");
+}
